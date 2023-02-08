e54c225ffdb48ab030f1cc9b73795b9f4c12c0e8
Samip <ghimiresamip91@gmail.com>
2023-02-08 17:00:47 +0545

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,30 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
-module.exports = {
-  siteMetadata: {
-    title: `blog`,
-    siteUrl: `https://www.yourdomain.tld`,
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const routes = [
+  {
+    type: "article",
+    path: "/articles/:uid",
   },
-  plugins: [],
+  {
+    type: "page",
+    path: "/:uid",
+  },
+];
+
+module.exports = {
+  plugins: [
+    // ...
+    'gatsby-plugin-image',
+    {
+      resolve: 'gatsby-source-prismic',
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+        routes,
+      },
+    },
+  ] 
 }
